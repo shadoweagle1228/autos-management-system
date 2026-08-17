5ef54c4eeb0aad83a748a454dd6d9d0f6bc6d8d3
Edwin Ramirez <earo1228@gmail.com>
2026-08-16 19:12:17 -0500

    se realiza carga iinicial de front y back

---
+++ user-auth-service/src/test/java/com/autos/user_auth_service/UserAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autos.user_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
